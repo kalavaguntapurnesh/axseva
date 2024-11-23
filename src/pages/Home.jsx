2cc2c94feecaa4bc94a07031df969a4a73a9dfd2
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-11-23 21:12:23 +0530

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./../components/Hero";
+import { Footer } from "antd/es/layout/layout";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
