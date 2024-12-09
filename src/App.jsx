9df459b30be516dddaf455675f19717e50e14ec8
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-12-09 14:18:53 +0530

    Changes_in_Collaborate_Us_Page

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,6 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Lottie from "lottie-react";
-import { useState } from "react";
-import Loading from "./assets/Loading.json";
 import CookieConsent from "./components/CookieConsent";
 import PrivacyPolicy from "./pages/Privacy";
 import Industries from "./pages/Industries";
@@ -20,25 +17,10 @@ import Benefits from "./pages/Benefits";
 import MicrosoftDynamics365 from "./pages/Services/MicrosoftDynamics365";
 import MicrosoftDynamics365Finance from "./pages/Services/MicrosoftDynamics365Finance";
 import TermsAndConditions from "./pages/TermsAndConditions";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
-  // const [isLoading, setIsLoading] = useState(true);
-
-  // setTimeout(() => {
-  //   setIsLoading(false);
-  // }, 1500);
   return (
     <BrowserRouter>
-      {/* {isLoading ? (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-          <div className="flex justify-center items-center">
-            <Lottie
-              animationData={Loading}
-              loop={true}
-              className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-auto"
-            />
-          </div>
-        </div>
-      ) : ( */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/privacy-policy" element={<PrivacyPolicy />}></Route>
@@ -49,6 +31,7 @@ function App() {
         <Route path="/dynamics-365-support-services" element={<Support />} />
         <Route path="/collaborate-with-us" element={<Benefits />} />
         <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="*" element={<PageNotFound />}></Route>
         <Route
           path="/services-microsoft-dynamics-365"
           element={<MicrosoftDynamics365 />}
@@ -58,8 +41,7 @@ function App() {
           element={<MicrosoftDynamics365Finance />}
         />
       </Routes>
-      {/* )} */}
-      <CookieConsent /> {/* Add CookieConsent here */}
+      <CookieConsent />
     </BrowserRouter>
   );
 }
